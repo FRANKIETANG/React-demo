2730dee72d00b041adcd1ee0fdfecc34001f5d88
frankietang <350558468@qq.com>
2017-08-20 23:49:09 +0800

    build

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-    <h1>Hi, world!</h1>,
+    <h1>Hello, world!</h1>,
     document.getElementById('root')
 )
