b44ecb73c7dded12821f1bab6f566a45d132f4ef
frankietang <350558468@qq.com>
2017-08-21 17:56:11 +0800

    <Welcome/>

--- src/index.js
+++ src/index.js
@@ -4,17 +4,13 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-function tick() {
-    const element = (
-        <div>
-            <h1>Hello world</h1>
-            <h2>It is {new Date().toLocaleTimeString()}.</h2>
-        </div>
-    )
-    ReactDOM.render(
-        element,
-        document.getElementById('root')
-    )
+class Welcome extends React.Component {
+    render() {
+        return <h1>Component</h1>
+    }
 }
 
-setInterval(tick, 1000)
+ReactDOM.render(
+    <Welcome/>,
+    document.getElementById('root')
+)
