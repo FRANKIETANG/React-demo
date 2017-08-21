f5ea27daff2f6a23c3319ecbba77ae41b79c418e
frankietang <350558468@qq.com>
2017-08-21 20:01:16 +0800

    哪些钩子里面可以加 setState

--- src/Welcome.js
+++ src/Welcome.js
@@ -7,10 +7,12 @@ class Welcome extends React.Component {
             date: new Date(),
             test: '1'
         }
-        this.setState({
-            date: new Date(),
-            test: 'constructor'
-        }) 
+        setInterval(function(){
+            this.setState({
+                date: new Date(),
+                test: 'setInterval'
+            })            
+        }.bind(this),5000)
         console.log('我已经在 constructor 里将 props 和 state 初始化好了')      
     }
     componentWillMount(){
@@ -21,10 +23,6 @@ class Welcome extends React.Component {
         console.log('运行到这里的话，说明马上就要运行 render 了')
     }  
     render() {
-        this.setState({
-            date: new Date(),
-            test: 'render'
-        })
         console.log('嗯，这里是 render')
         return (
             <div>
@@ -49,22 +47,8 @@ class Welcome extends React.Component {
         })
     }
     shouldComponentUpdate(){
-        this.setState({
-        date: new Date(), // 更新 date
+        return true
         test: 'shouldComponentUpdate'
-        })
-    }
-    componentWillUpdate(){
-        this.setState({
-        date: new Date(), // 更新 date
-        test: 'componentWillUpdate'
-        })
-    }
-    componentDidUpdate(){
-        this.setState({
-        date: new Date(), // 更新 date
-        test: 'componentDidUpdate'
-        })
     }
     componentWillUnmount(){
         console.log('要死了')
